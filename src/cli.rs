3352578cd8e4edcc2fd400cbde26ab8026f47ec9
Shantanu Goel <shantanu.goel@gmail.com>
2021-06-09 22:24:53 +0530

    Add subcommands

--- src/cli.rs
+++ src/cli.rs
@@ -3,7 +3,8 @@ use clap::{AppSettings, Clap};
 /// Send a system notification
 #[derive(Clap)]
 #[clap(name = "sup", author = "Shantanu Goel <shantanugoel.com>")]
-#[clap(setting = AppSettings::ColoredHelp)]
+#[clap(setting = AppSettings::ColoredHelp,
+     setting=AppSettings::DeriveDisplayOrder)]
 pub struct Opts {
     /// Notify with this message
     #[clap(short, long, default_value = "Hey!")]
@@ -13,9 +14,37 @@ pub struct Opts {
     #[clap(short, long)]
     pub run: Option<String>,
 
-    /// Where should the notification be sent to
-    #[clap(short, long, default_value = "local")]
-    pub destination: String,
+    #[clap(subcommand)]
+    subcmd: SubCommand,
+}
+
+/// Notifications should be sent to local system
+#[derive(Clap)]
+enum SubCommand {
+    Local(Local),
+    Telegram(Telegram),
+}
+
+#[derive(Clap)]
+struct Local {}
+
+#[derive(Clap)]
+struct Telegram {
+    /// Telegram chat id to which notification should be sent.
+    #[clap(long)]
+    pub tg_chat_id: String,
+
+    /// Specify Telegram bot token in command
+    #[clap(
+        long,
+        conflicts_with("tg-bot-token-env"),
+        required_unless_present("tg-bot-token-env")
+    )]
+    pub tg_bot_token: Option<String>,
+
+    /// Take Telegram bot token from SUP_TG_BOT_TOKEN env variable
+    #[clap(long, required_unless_present("tg-bot-token"))]
+    pub tg_bot_token_env: bool,
 }
 
 pub fn parse_opts() -> Opts {
